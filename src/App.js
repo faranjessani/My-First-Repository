36bb595da806bc7755b3ce4b017a23f596760083
Faran Jessani <faran@jessani.com>
2021-10-18 17:10:18 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello David, Anton, Itzel, Mahdiya, and Denny</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
