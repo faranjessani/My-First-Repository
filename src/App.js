1d9c5cc787d2dabdd19a90e4b6d4b5dbe33c816f
Faran Jessani <faran@jessani.com>
2021-10-18 17:12:06 -0400

    Add year and school information.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello David, Anton, Itzel, Mahdiya, and Denny</h1>
+      <h2>2021 - Queens Technical</h2>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
